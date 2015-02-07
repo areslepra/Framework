5e0a7b7f0d6a150fdc6394242ba0c01964ae7d9f
Cody Roodaka <roodakazo@gmail.com>
2015-02-07 12:02:04 -0300

    Ajuste de sesiones
    Agregadas variables a su archivo de configuración.

--- configurations/session.php
+++ configurations/session.php
@@ -1,9 +1,12 @@
 <?php defined('ROOT') or exit('No tienes Permitido el acceso.');
 return array(
+ 'session_table' => '',
  'duration' => 604800,
-  'algorithm' => 'md5',
-  'cookie_life' => 300, // cinco minutos
-  'cookie_name' => 'micookiepiola',
-  'cookie_path' => DS,
-  'cookie_domain' => $_SERVER['SERVER_NAME']
+ 'algorithm' => 'md5',
+ 'user_object' => null,
+ 'user_fields' => null,
+ 'cookie_life' => 300, // cinco minutos
+ 'cookie_name' => 'micookiepiola',
+ 'cookie_path' => DS,
+ 'cookie_domain' => $_SERVER['SERVER_NAME']
  );
